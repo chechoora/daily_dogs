3a51496bc3cec84eaa41f550d36de257bed3f2e2
kyrylokharchenko <kharchenko.kir@gmail.com>
2023-09-13 12:28:51 +0300

    [-] Added Apple Watch extention.

---
+++ ios/daily_watch_dogs Watch App/daily_watch_dogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  daily_watch_dogsApp.swift
+//  daily_watch_dogs Watch App
+//
+//  Created by Kyrylo Kharchenko on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct daily_watch_dogs_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
